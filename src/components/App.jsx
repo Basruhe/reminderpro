3e11d9dcec64f24273f9b8eba1216b6f5b6ac41d
Basruhe <sb.ruhe@gmail.com>
2020-03-13 00:21:06 +0100

    just starting with connect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,18 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ""
+    };
+  }
+
+  addReminder() {
+    console.log("this.state", this.state);
+  }
+
   render() {
     return (
       <div className="App">
@@ -8,9 +20,17 @@ class App extends Component {
 
         <div className="form-inline">
           <div className="form-group">
-            <input className="form-control" placeholder="I have to.." />
+            <input
+              className="form-control"
+              placeholder="I have to.."
+              onChange={event => this.setState({ text: event.target.value })}
+            />
           </div>
-          <button type="button" className="btn btn-success">
+          <button
+            type="button"
+            className="btn btn-success"
+            onClick={() => this.addReminder()}
+          >
             Add reminder
           </button>
         </div>
