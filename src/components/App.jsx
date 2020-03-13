e9eb824b7eafae2e91e6a9975fda0c671edddb81
Basruhe <sb.ruhe@gmail.com>
2020-03-13 13:21:20 +0100

    working on redux connect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { addReminder } from "../actions";
 
 class App extends Component {
   constructor(props) {
@@ -10,7 +12,8 @@ class App extends Component {
   }
 
   addReminder() {
-    console.log("this.state", this.state);
+    console.log("this", this);
+    this.props.addReminder(this.state.text);
   }
 
   render() {
@@ -39,4 +42,12 @@ class App extends Component {
   }
 }
 
-export default App;
+function mapStateToProps(state) {
+  console.log("state", state);
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ addReminder }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
