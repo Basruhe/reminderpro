62995336162fc65d8e3fbba7490aaa9e1fb43016
Basruhe <sb.ruhe@gmail.com>
2020-04-14 17:53:22 +0200

    progress save. task list ui

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,6 +16,13 @@ class App extends Component {
     this.props.addReminder(this.state.text);
   }
 
+  renderReminders() {
+    const { reminders } = this.props;
+  }
+
+  //current:
+  //https://www.skillshare.com/classes/ReactJS-and-Redux-Build-4-Web-Apps-34/589612490/projects?via=watch-history
+
   // renderReminders() {
   //   const { reminders } = this.props;
   //   // this is not working; as nothing is (somehow) ever added to the reminders in the reducers index.js
@@ -32,9 +39,9 @@ class App extends Component {
   //     </ul>
   //   );
   // }
-  //
 
   render() {
+    // console.log("this.props", this.props);
     return (
       <div className="App">
         <div className="title">Reminder PRO!</div>
@@ -47,7 +54,7 @@ class App extends Component {
               onChange={event => this.setState({ text: event.target.value })}
             />
           </div>
-          {/* {this.renderReminders()} */}
+          {this.renderReminders()}
           <button
             type="button"
             className="btn btn-success"
@@ -62,13 +69,13 @@ class App extends Component {
 }
 
 // Now, we need to mapstatetoprops so that we can update the redux state with our action and recognize it in our UI.
-// function mapStateToProps(state) {
-//   console.log("state", state);
-// }
+function mapStateToProps(state) {
+  console.log("state", state);
+}
 
 // This is now redundant, since addreminder is also added to the export default connect. This is a shortcut. Otherwise you'd need to write an actual mapdispatchtoprops like below, and import bindactioncreators as well
 // function mapDispatchToProps(dispatch) {
 //   return bindActionCreators({ addReminder }, dispatch);
 // }
 
-export default connect(null, { addReminder })(App);
+export default connect(mapStateToProps, { addReminder })(App);
